97c9c65299853d75e550645e3f93f475c2c6f434
Ph6767 <phoophano.6@gmail.com>
2026-04-27 06:26:58 +0000

    merge

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,1 +1,7 @@
-
+fuckpython = int(input())
+if fuckpython > 0:
+    print('This number is positive.')
+elif fuckpython == 0:
+    print('This number is both positive and negative.')
+else:
+    print('This number is negative.')

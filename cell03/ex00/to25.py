119725ba1002b07d577549cf7318ef6f8678a6ef
Ph6767 <phoophano.6@gmail.com>
2026-04-27 14:21:21 +0000

    ex00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number less than 25 : " ))
+if(x < 25):
+    while(x < 25):
+        x += 1
+        print ("Inside the loop, my variable is ", + x)
+else:
+    print("Error")
